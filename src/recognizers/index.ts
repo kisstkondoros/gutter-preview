5b2287a4f8282300e6e182892eca806b2e469699
Kiss Tamás <kisstkondoros@gmail.com>
2018-09-10 21:38:31 +0200

    Add localLinkRecognizer

--- src/recognizers/index.ts
+++ src/recognizers/index.ts
@@ -4,11 +4,13 @@ import { urlRecognizer } from './urlrecognizer';
 import { imgageSourceRecognizer } from './imagesourcerecognizer';
 import { pythonRecognizer } from './pythonrecognizer';
 import { linkRecognizer } from './linkrecognizer';
+import { localLinkRecognizer } from './locallinkrecognizer';
 
 export const recognizers: ImagePathRecognizer[] = [
     markdownRecognizer,
     urlRecognizer,
     linkRecognizer,
+    localLinkRecognizer,
     imgageSourceRecognizer,
     pythonRecognizer
 ];
---
+++ src/recognizers/locallinkrecognizer.ts
@@ -0,0 +1,51 @@
+const pathPrefix = '(\\.\\.?|\\~)';
+const pathSeparatorClause = '\\/';
+// '":; are allowed in paths but they are often separators so ignore them
+// Also disallow \\ to prevent a catastropic backtracking case #24798
+const excludedPathCharactersClause = '[^\\0\\s!$`&*()\\[\\]+\'":;\\\\]';
+/** A regex that matches paths in the form /foo, ~/foo, ./foo, ../foo, foo/bar */
+const unixLocalLinkClause =
+    '((' +
+    pathPrefix +
+    '|(' +
+    excludedPathCharactersClause +
+    ')+)?(' +
+    pathSeparatorClause +
+    '(' +
+    excludedPathCharactersClause +
+    ')+)+)';
+
+const winDrivePrefix = '[a-zA-Z]:';
+const winPathPrefix = '(' + winDrivePrefix + '|\\.\\.?|\\~)';
+const winPathSeparatorClause = '(\\\\|\\/)';
+const winExcludedPathCharactersClause = '[^\\0<>\\?\\|\\/\\s!$`&*()\\[\\]+\'":;]';
+/** A regex that matches paths in the form c:\foo, ~\foo, .\foo, ..\foo, foo\bar */
+const winLocalLinkClause =
+    '((' +
+    winPathPrefix +
+    '|(' +
+    winExcludedPathCharactersClause +
+    ')+)?(' +
+    winPathSeparatorClause +
+    '(' +
+    winExcludedPathCharactersClause +
+    ')+)+)';
+
+const baseLocalLinkClause = process.platform === 'win32' ? winLocalLinkClause : unixLocalLinkClause;
+// Append line and column number regex
+const _localLinkPattern = new RegExp(`${baseLocalLinkClause}`);
+
+import { ImagePathRecognizer } from './recognizer';
+import { TextDocument } from 'vscode';
+
+export const localLinkRecognizer: ImagePathRecognizer = {
+    recognize: (document: TextDocument, line: string) => {
+        let match = _localLinkPattern.exec(line);
+        let imagePath: string;
+
+        if (match && match.length > 1) {
+            imagePath = match[1];
+        }
+        return imagePath;
+    }
+};
