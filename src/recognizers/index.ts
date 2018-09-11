e3f71876c3080fc37968f27ecb4e69e20bb67ba8
Kiss Tamás <kisstkondoros@gmail.com>
2018-09-11 17:50:46 +0200

    Remove superfluous recognizers

--- src/recognizers/index.ts
+++ src/recognizers/index.ts
@@ -1,16 +1,5 @@
-import { markdownRecognizer } from './markdownrecognizer';
 import { ImagePathRecognizer } from './recognizer';
-import { urlRecognizer } from './urlrecognizer';
-import { imgageSourceRecognizer } from './imagesourcerecognizer';
-import { pythonRecognizer } from './pythonrecognizer';
 import { linkRecognizer } from './linkrecognizer';
 import { localLinkRecognizer } from './locallinkrecognizer';
 
-export const recognizers: ImagePathRecognizer[] = [
-    markdownRecognizer,
-    urlRecognizer,
-    linkRecognizer,
-    localLinkRecognizer,
-    imgageSourceRecognizer,
-    pythonRecognizer
-];
+export const recognizers: ImagePathRecognizer[] = [linkRecognizer, localLinkRecognizer];
