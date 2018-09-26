9c917b83b2c4f6c5733f807b4d6f5f9c0d20750a
Kiss Tamás <kisstkondoros@gmail.com>
2018-09-26 20:16:45 +0200

    Use more flexible pattern for data url detection

--- src/recognizers/dataurlrecognizer.ts
+++ src/recognizers/dataurlrecognizer.ts
@@ -2,18 +2,32 @@ import { ImagePathRecognizer, UrlMatch } from './recognizer';
 
 export const dataUrlRecognizer: ImagePathRecognizer = {
     recognize: (lineIndex: number, line: string): UrlMatch[] => {
-        let pattern: RegExp = /(data:image(\/[a-z0-9-+.]+(;[a-z0-9-.!#$%*+.{}|~`]+=[a-z0-9-.!#$%*+.{}|~`]+)*)?(;base64)?,([a-z0-9!$&',()*+;=\-._~:@\/?%\s]*))[\"\'\)]+/gim;
+        const urlPrefixLength = "url('".length;
+
+        let patternWithSingleQuote: RegExp = /url\(\'(data:image.*)\'\)/gim;
+        let patternWithDoubleQuote: RegExp = /url\(\"(data:image.*)\"\)/gim;
 
         let match: RegExpExecArray;
         const result = [];
-        while ((match = pattern.exec(line))) {
+        while ((match = patternWithSingleQuote.exec(line))) {
             if (match.length > 1) {
                 const imagePath = match[1];
                 result.push({
                     url: imagePath,
                     lineIndex,
-                    start: match.index,
-                    end: match.index + imagePath.length
+                    start: match.index + urlPrefixLength,
+                    end: match.index + urlPrefixLength + imagePath.length
+                });
+            }
+        }
+        while ((match = patternWithDoubleQuote.exec(line))) {
+            if (match.length > 1) {
+                const imagePath = match[1];
+                result.push({
+                    url: imagePath,
+                    lineIndex,
+                    start: match.index + urlPrefixLength,
+                    end: match.index + urlPrefixLength + imagePath.length
                 });
             }
         }
