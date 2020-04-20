853f01231c659a086afd108d65b2841b61340369
Kiss Tamás <kisstkondoros@gmail.com>
2020-04-20 22:15:34 +0200

    Adjust markdown resolver to support optional title attributes too

--- src/recognizers/markedlinkrecognizer.ts
+++ src/recognizers/markedlinkrecognizer.ts
@@ -2,12 +2,12 @@ import { ImagePathRecognizer, UrlMatch } from './recognizer';
 
 export const markedLinkRecognizer: ImagePathRecognizer = {
     recognize: (lineIndex: number, line: string): UrlMatch[] => {
-        let pattern: RegExp = /(\[.*?\])\((.*?)\)/gi;
+        let pattern: RegExp = /(\[.*\])\(([^"]*)(\".*\")?\)/gi;
         let match: RegExpExecArray;
         const result = [];
         while ((match = pattern.exec(line))) {
             if (match.length > 0) {
-                const imagePath = match[2];
+                const imagePath = match[2].trim();
                 const matchIndex = match.index + match[1].length + 1;
                 result.push({
                     url: imagePath,
