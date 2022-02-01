c8c362602a536c62d9cef4e284a3a82847487bfe
Ian Obermiller <ian@obermillers.com>
2022-02-01 09:58:03 -0500

    Update regex for data urls
    Make the regex for data urls simpler and work outside of CSS files

--- src/recognizers/dataurlrecognizer.ts
+++ src/recognizers/dataurlrecognizer.ts
@@ -2,32 +2,18 @@ import { ImagePathRecognizer, UrlMatch } from './recognizer';
 
 export const dataUrlRecognizer: ImagePathRecognizer = {
     recognize: (lineIndex: number, line: string): UrlMatch[] => {
-        const urlPrefixLength = "url('".length;
-
-        let patternWithSingleQuote: RegExp = /url\(\'(data:image.*)\'\)/gim;
-        let patternWithDoubleQuote: RegExp = /url\(\"(data:image.*)\"\)/gim;
+        const pattern = /data:[^'")]+/gm;
 
         let match: RegExpExecArray;
         const result = [];
-        while ((match = patternWithSingleQuote.exec(line))) {
-            if (match.length > 1) {
-                const imagePath = match[1];
+        while ((match = pattern.exec(line))) {
+            if (match) {
+                const imagePath = match[0];
                 result.push({
                     url: imagePath,
                     lineIndex,
-                    start: match.index + urlPrefixLength,
-                    end: match.index + urlPrefixLength + imagePath.length,
-                });
-            }
-        }
-        while ((match = patternWithDoubleQuote.exec(line))) {
-            if (match.length > 1) {
-                const imagePath = match[1];
-                result.push({
-                    url: imagePath,
-                    lineIndex,
-                    start: match.index + urlPrefixLength,
-                    end: match.index + urlPrefixLength + imagePath.length,
+                    start: match.index,
+                    end: match.index + imagePath.length,
                 });
             }
         }
