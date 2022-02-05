b70736285597025e694d6de245e7635aeff6f029
Kiss Tamás <kisstkondoros@gmail.com>
2022-02-05 14:41:45 +0100

    Handle non-base64 encoded Data URIs as well

--- src/recognizers/dataurlrecognizer.ts
+++ src/recognizers/dataurlrecognizer.ts
@@ -8,12 +8,24 @@ export const dataUrlRecognizer: ImagePathRecognizer = {
         let patternWithDoubleQuote: RegExp = /url\(\"(data:image.*)\"\)/gim;
 
         let match: RegExpExecArray;
+
+        let escapeURIContent = (content: string) => {
+            if (content.indexOf(' ') > 0 || content.indexOf('"') || content.indexOf("'") > 0) {
+                return (
+                    content.substring(0, content.indexOf(',') + 1) +
+                    encodeURIComponent(content.substring(content.indexOf(',') + 1))
+                );
+            } else {
+                return content;
+            }
+        };
+
         const result = [];
         while ((match = patternWithSingleQuote.exec(line))) {
             if (match.length > 1) {
                 const imagePath = match[1];
                 result.push({
-                    url: imagePath,
+                    url: escapeURIContent(imagePath),
                     lineIndex,
                     start: match.index + urlPrefixLength,
                     end: match.index + urlPrefixLength + imagePath.length,
@@ -24,7 +36,7 @@ export const dataUrlRecognizer: ImagePathRecognizer = {
             if (match.length > 1) {
                 const imagePath = match[1];
                 result.push({
-                    url: imagePath,
+                    url: escapeURIContent(imagePath),
                     lineIndex,
                     start: match.index + urlPrefixLength,
                     end: match.index + urlPrefixLength + imagePath.length,
